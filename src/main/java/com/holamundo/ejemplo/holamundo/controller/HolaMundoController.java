1ba6523c67e33de94254469ae9fa773178062928
Matias1402 <matias18torres18@gmail.com>
2025-04-18 22:45:17 -0400

    mas a

--- src/main/java/com/holamundo/ejemplo/holamundo/controller/HolaMundoController.java
+++ src/main/java/com/holamundo/ejemplo/holamundo/controller/HolaMundoController.java
@@ -8,6 +8,6 @@ public class HolaMundoController {
 
     @GetMapping("/hola")
     public String holaMundo() {
-        return "!HOLA MUNDO!!!!!!";
+        return "!HOLAaaaaa MUNDO!!!!!!";
     }
 }
