89b8c19aaf0e0bc8ef6e4ba93541b78cda065595
Matias1402 <matias18torres18@gmail.com>
2025-04-22 14:56:00 -0400

    Modificacion Api

--- src/main/java/com/holamundo/ejemplo/holamundo/controller/ClienteController.java
+++ src/main/java/com/holamundo/ejemplo/holamundo/controller/ClienteController.java
@@ -1,4 +1,16 @@
 package com.holamundo.ejemplo.holamundo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
 public class ClienteController {
+
+    @GetMapping("/clientes")
+    public String index(){
+        return "Bienvenido, este es el listado de clientes";
+    }
+
 }
--- src/main/java/com/holamundo/ejemplo/holamundo/controller/ProductoController.java
+++ src/main/java/com/holamundo/ejemplo/holamundo/controller/ProductoController.java
@@ -1,4 +1,56 @@
 package com.holamundo.ejemplo.holamundo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/v2")
 public class ProductoController {
+
+    @GetMapping("/productos")
+    public String index(){
+        return "Bienvenido, este es el listado de productos";
+    }
+    private List<ArrayList<Object>> productos = new ArrayList<>();
+
+
+    public ProductoController() {
+
+        ArrayList<Object> producto1 = new ArrayList<>();
+        producto1.add(1);
+        producto1.add("Computador");
+        producto1.add(799.99);
+
+        ArrayList<Object> producto2 = new ArrayList<>();
+        producto2.add(2);
+        producto2.add("Telefono");
+        producto2.add(499.99);
+
+        ArrayList<Object> producto3 = new ArrayList<>();
+        producto3.add(3);
+        producto3.add("Tablet");
+        producto3.add(299.99);
+
+
+        productos.add(producto1);
+        productos.add(producto2);
+        productos.add(producto3);
+    }
+
+    @GetMapping("/productos/{idProducto}")
+    public String buscar(@PathVariable int idProducto) {
+        for (ArrayList<Object> producto : productos) {
+            if (producto.get(0).equals(idProducto)) {
+                return "Producto encontrado: ID: " + producto.get(0) + ", Nombre: " + producto.get(1) + ", Precio: " + producto.get(2);
+            }
+        }
+        return "Error 404: Producto no encontrado";
+    }
+
 }
